c6d2f0b693a0925de2704017954f88b4c0c126bd
Alexey Knyazev <joalexey@gmail.com>
2019-04-30 16:37:25 +0300

    webp for content images implemented. svg sprite for inline svg implemented. TO-DO: implement external svg sprite in css

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,10 @@ gulp.task("webp", function () {
   });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src([
+    "source/img/icon-*.svg",
+    "source/img/logo-htmlacademy.svg"
+  ])
     .pipe(svgstore({
       inlineSvg: true
     }))
